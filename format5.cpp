140aee1ddaba52027b91fecd9df80a6d6a97e444
siddup4488 <santhoshkrishna4488@gmail.com>
2020-02-26 12:44:11 +0530

    First commit

---
+++ format5.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+main()
+{
+	int r,sp,p,no,n;
+	printf("enter numer of rows:\n");
+	scanf("%d",&no);
+	n=no;
+	for(r=1;r<=no;r++)
+	{
+		for(sp=1;sp<=n;sp++)
+		
+		{
+			printf(" ");
+			
+			
+		}
+		n--;
+		for(p=1;p<=r;p++)
+		{
+			printf("%d ",r);
+			
+		}
+		printf("\n");
+	}
+}
+
